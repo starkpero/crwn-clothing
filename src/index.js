5b0ad2255cde686afea0b770374bf5242248d395
stark <anishgupta0066@gmail.com>
2021-08-18 01:05:56 +0530

    moved the directory and shop data into its own reducer,created respective selectors and updated corresponding components with new ux flow for directory/shop

--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter,Link} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import {store} from './redux/store';
+import {persistor} from './redux/store';
 
 ReactDOM.render(
   <Provider store ={store}>
   <React.StrictMode>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
     </BrowserRouter>
   </React.StrictMode>
   </Provider>,
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import SHOP_DATA from './2.3 shop.data';
-import PreviewCollection from '../../components/collection-preview/collection-preview.component';
+import { Route } from 'react-router';
+//import SHOP_DATA from './2.3 shop.data';
+//import PreviewCollection from '../../components/collection-preview/collection-preview.component';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component';
+import CollectionPage from '../collection/collection.component';
+// import {connect} from 'react-redux';
+// import { createStructuredSelector } from 'reselect';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super();
-
-        this.state = {
-            collections : SHOP_DATA
-        }
-    }
-
-
-    render() {
-        const { collections } = this.state;
-        return (
+const ShopPage = ({match})=>(
           <div className='shop-page'>
-            {collections.map(({ id, ...otherCollectionProps }) => (
-              <PreviewCollection key={id} {...otherCollectionProps} />
-            ))}
+            <Route exact path={`${match.path}`} component={CollectionOverview}/>
+            <Route  path={`${match.path}/:collectionId`} component={CollectionPage}/>
           </div>
-        );
-      }
-}
+        );  
+      
+
 
 
 export default ShopPage;
